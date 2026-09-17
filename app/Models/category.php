9c454450efe5683fd443505b517a51a78c1d463b
Gabriela Castro <gabrielacastro@MacBook-Air-de-Gabriela.local>
2026-09-17 11:23:56 -0500

    Paso 3 taller

--- app/Models/category.php
+++ app/Models/category.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class category extends Model
+class Category extends Model
 {
     use HasFactory;
+
     protected $table = 'categories';
+
     protected $primaryKey = 'id';
 
+    protected $fillable = [
+        'name',
+    ];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Models/product.php
+++ app/Models/product.php
@@ -4,10 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class product extends Model
+class Product extends Model
 {
     use HasFactory;
+
     protected $table = 'products';
+
     protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category_id',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
     }
+}
